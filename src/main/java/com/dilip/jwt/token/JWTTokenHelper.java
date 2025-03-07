5e9a0837fa53becbaf808cbabf97b8a511925a2c
Dilip IT Academy <33460657+DilipItAcademy@users.noreply.github.com>
2025-03-07 21:37:47 +0530

    Update JWTTokenHelper.java

--- src/main/java/com/dilip/jwt/token/JWTTokenHelper.java
+++ src/main/java/com/dilip/jwt/token/JWTTokenHelper.java
@@ -2,42 +2,62 @@
 package com.dilip.jwt.token;
 
 import java.util.Date;
-
+import javax.crypto.SecretKey;
 import org.springframework.stereotype.Component;
-
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JWTTokenHelper {
 	
-    public static final long JWT_TOKEN_VALIDITY_MILLIS =  5 * 60000; // 5 mins
-    private String secret = "afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf";
+ 	// Secret key
+	private final String SECRET_KEY = "ajjfoffhsifgwe87wyhwr7rhuhy8hfifnhx7xjdhnaih77373hff34rdijcnhkcbndkhcbkcbkjcbcbxhjcbhdkcbdu";
+	private final long TOKEN_EXPIRY_DURATION = 5 * 60000; // 5 mins // 1sec = 1000 ms -> 1 min = 60000 ms
 
-   
-    //retrieve username from jwt token
-    public String getUsernameFromToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
-    }
+	private SecretKey getSecretKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
 
-    //check if the token has expired
-    private Boolean isTokenExpired(String token) {
-    	final Date expiration = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getExpiration();
-        return expiration.before(new Date());
-    }
+	public String createToken(String emailId) {
 
-    //generate token for user
-    //Sign the JWT using the HS512 algorithm and secret key.
-    public String generateToken(String userName) {
-    	 return Jwts.builder().setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
-                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY_MILLIS))
-                 .signWith(SignatureAlgorithm.HS512, secret).compact();
-    }
 
-    //validate token
-    public Boolean validateToken(String token, String userName) {
-        final String username = getUsernameFromToken(token);
-        return (username.equals(userName) && !isTokenExpired(token));
-    }
+		String token = Jwts.builder()
+				.subject(emailId) // unique user id
+				.issuedAt(new Date(System.currentTimeMillis())) // setting creation time
+				.expiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRY_DURATION)) // setting Expire time
+				.signWith(getSecretKey())
+				.compact();
 
+		return token;
+	}
+
+	public boolean isValidToken(String token, String userId) {
+		String userIDFromToken = getUserIdFromToken(token);
+		System.out.println("User Id Retrived From Token : " + userIDFromToken);
+		return userIDFromToken.equalsIgnoreCase(userId) && isTokenExpired(token);
+	}
+
+	// user Id from the token
+	public String getUserIdFromToken(String token) {
+		return Jwts.parser()
+				.verifyWith(getSecretKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload()
+				.getSubject();
+	}
+
+	// check if the token has expired
+	private boolean isTokenExpired(String token) {
+		Date expirtyTime = Jwts.parser()
+						.verifyWith(getSecretKey())
+						.build()
+						.parseSignedClaims(token)
+						.getPayload()
+				.getExpiration();
+		System.out.println("Token Epirty Time : " + expirtyTime);
+		return expirtyTime.after(new Date());
+	}
 }
